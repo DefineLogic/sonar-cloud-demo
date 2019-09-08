c711ecbddffbc45bae7cdcce44fbb8326ab495f6
kamal <comeallaalay@gmail.com>
2019-09-08 23:31:56 +0530

    added RestDemoController

---
+++ src/main/java/com/example/sonarclouddemo/DemoRestController.java
@@ -0,0 +1,16 @@
+package com.example.sonarclouddemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+    @RequestMapping(value = "/add")
+    public ResponseEntity<String> addAss(@RequestParam("test")String text){
+        String result = text.toUpperCase();
+        return ResponseEntity.ok(result);
+    }
+}
--- src/main/java/com/example/sonarclouddemo/SonarCloudDemoApplication.java
+++ src/main/java/com/example/sonarclouddemo/SonarCloudDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SonarCloudDemoApplication {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		SpringApplication.run(SonarCloudDemoApplication.class, args);
-	}
+		}
 
-}
+		}
